63939a490cb47ad788cb41dcd557abd442056975
Kwesi Parry Apponsah <kwesi@oddjob.utias.utoronto.ca>
2016-01-30 06:41:49 -0500

    Add text to README file

--- db/data/tasks.py
+++ db/data/tasks.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from celery import shared_task
 
 # import app utils
-from data.utils import symbols, prices
+from data.utils import symbols, prices, exchanges
 
 @shared_task
 def update_prices():
@@ -18,3 +18,11 @@ def update_securities_symbols():
 @shared_task
 def add_historical_prices():
     prices.add_historical_prices()
+
+@shared_task
+def add_prices_for_tickers(tickers, start_date=None):
+    prices.add_prices_for_tickers(tickers, start_date)
+
+@shared_task
+def create_exchanges():
+    exchanges.create_exchanges()
